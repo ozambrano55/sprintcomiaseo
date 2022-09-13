fc259ef1738817bd9a122dd75109b90cd550d0c1
ozambrano55 <56766582+ozambrano55@users.noreply.github.com>
2022-09-13 10:41:47 -0500

    m13/09/2022 para descargar oficina

--- src/main/java/com/example/sprintcomiaseo/entity/Usuario.java
+++ src/main/java/com/example/sprintcomiaseo/entity/Usuario.java
@@ -1,7 +1,11 @@
 package com.example.sprintcomiaseo.entity;
 
-import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+@Getter
+@Setter
 @Entity
 public class Usuario {
     @Id
@@ -16,43 +20,4 @@ public class Usuario {
     @OneToOne
     private Cliente cliente;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getClave() {
-        return clave;
-    }
-
-    public void setClave(String clave) {
-        this.clave = clave;
-    }
-
-    public boolean isVigencia() {
-        return vigencia;
-    }
-
-    public void setVigencia(boolean vigencia) {
-        this.vigencia = vigencia;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
 }
